c042825ad8c38e65b14af47c668a52e35398697a
LeeBhin <bini4039@naver.com>
2024-02-29 13:56:19 +0900

    firebase env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfxXv1KgujYLD5B5nZSIBjaTLtytgwEyc",
-    authDomain: "boasting-778b9.firebaseapp.com",
-    projectId: "boasting-778b9",
-    storageBucket: "boasting-778b9.appspot.com",
-    messagingSenderId: "897116949789",
-    appId: "1:897116949789:web:9c8f40e6835a33c17765f4",
-    measurementId: "G-BB9JRRQJMG"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
